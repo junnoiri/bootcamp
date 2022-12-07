c5331da94a5d42639c24c21b0e004ed3524a8c5f
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2022-12-07 16:53:21 -0600

    modify the slell script

--- shell/shellscript_use_loops.sh
+++ shell/shellscript_use_loops.sh
@@ -3,37 +3,29 @@
 #for文
 
 #数値を繰り返す
-for i in 1 2 3 4 5 6 7 8 9
+for index in 1 2 3 4 5 6 7 8 9
 do
-  echo $i
+  echo "${index}"
 done
 
 #文字列を繰り返す
-for i in cabbage lettuce onion broccoli carrots
+for index in 'cabbage' 'lettuce' 'onion' 'broccoli' 'carrots'
 do
-  echo $i
+  echo "${index}"
 done
 
 #seqで回数を指定する
-for i in `seq 15`
+for index in `seq 15`
 do
-  echo $i人
+  echo "${index}人"
 done
 
 #while文
 
-#回数を指定して繰り返す
-i=0
-while [ $i -ne 10 ]
+#コンソールから入力された文字列が"enginner"であれば、処理が終了する
+input=' '
+while [ $input != 'engineer' ]
 do
-  echo $i回目
-  i=`expr 1 + $i`
-done
-
-#コンソールから入力された文字列が"enginner"である限り、処理が実行する
-read input
-while [ $input = engineer ]
-do
-  echo Be engineer
+  echo 'Enter "engineer"'
   read input
 done
