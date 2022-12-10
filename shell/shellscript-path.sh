6396ef5048082bb658af6fd8d6e188c5e5fdf16b
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2022-12-10 07:03:09 -0600

    added a shell script to determine if a file path is relative or absolute

---
+++ shell/shellscript-path.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# ../bootcamp/shellscript-path.sh :相対パス
+# /Users/noiritonshisuke/Desktop/bootcamp/shell :絶対パス
+
+# 引数が渡されたことをif文で確認する
+if [ -n "$1" ]
+then
+  echo "Parameter passed = $1"
+  path="${1}"
+  
+  # pathの最初の文字列に"/"が最初にあれば、絶対パス
+  if [ "${path:0:1}" = "/" ]; then
+    echo 'The file path is absolute path.'
+  else 
+    echo 'The file path is relative path.'
+  fi
+
+else
+# パラメータが渡されていない場合、メッセージを出力
+  echo "No parameter passed"
+fi
