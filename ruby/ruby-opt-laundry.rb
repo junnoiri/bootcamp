300a8160d28942a088bda341b78008e41c163aac
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2022-12-20 13:57:21 -0600

    added a ruby file that creates landry object

---
+++ ruby/ruby-opt-laundry.rb
@@ -0,0 +1,38 @@
+class WashingMachine
+  @@cloth = 0
+
+  def initialize(laundry_capacity)
+    @laundry_capacity = []
+  end
+  
+  if cloth_number > 30 
+    puts "The washing machine is full."
+  else
+    
+  end
+
+  def initialize(quantity, price)
+    @quantity = quantity
+    @price = price
+  end
+
+  def buy
+    puts "りんごを#{@quantity}個買い、#{calculation(@quantity, @price)}円でした。"
+  end
+
+  private
+
+  def laundry()
+    
+  end
+end
+
+class Cloth
+  def initialize(condition)
+    @condition = condition
+  end
+end
+
+cloth1 = Cloth.new("dirty")
+cloth2 = Cloth.new("clean")
+
