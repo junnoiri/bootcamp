6c8e0c5b9f6d2ebdb3d64bbcf69ff7d3a0548c56
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2022-12-26 16:25:56 -0600

    updated the ruby file

--- command-line-tool/time_tracking_tool.rb
+++ command-line-tool/time_tracking_tool.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 class TrackTime
   attr_accessor :task_name, :start_time, :end_time, :actual_time
 
@@ -8,61 +10,64 @@ class TrackTime
 
   def record_log(work_time)
     # ログファイルを作成し、記録
-    @task_log = File.open("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt", "a")
-    @task_log.puts("#{work_time}")
-    @task_log.close
+    task_log = File.open("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt", "a")
+    task_log.puts("#{work_time}")
+    task_log.close
   end  
 
   # タスクの開始を記録
   def record_start_time
-    @log_start_time = "タスク(#{@task_name})開始時刻: #{@timestamp.strftime('%Y年%m月%d日 %H時%M分%S秒')}"
-    puts @log_start_time
-
+    log_start_time = "タスク(#{@task_name})開始時刻: #{@timestamp.strftime('%Y年%m月%d日 %H時%M分%S秒')}"
+    puts log_start_time
     # タスクの開始をログファイルに記録
-    record_log(@log_start_time)
+    record_log(log_start_time)
   end
 
   # タスクの終了を記録
   def record_end_time
-    @log_end_time = "タスク(#{@task_name})終了時刻: #{@timestamp.strftime('%Y年%m月%d日 %H時%M分%S秒')}"
-    puts @log_end_time
-
+    log_end_time = "タスク(#{@task_name})終了時刻: #{@timestamp.strftime('%Y年%m月%d日 %H時%M分%S秒')}"
+    puts log_end_time
     # タスクの終了をログファイルに記録
-    record_log(@log_end_time)
+    record_log(log_end_time)
   end
 
-  def find_todqys_log_file
-
+  def find_task_log
+    log_lines = []
+    File.foreach("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt"){ |line|
+      line = /: /.match(line).post_match.chomp
+      log_lines << Time.strptime(line,'%Y年%m月%d日 %H時%M分%S秒')
+    }
+    p log_lines
+    return  log_lines
   end
 
   # タスクの実績時間を記録
   def record_actual_time
-    File.open("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt").each do |line|
-      puts line
-      # next unless line =~ /開始時刻/ 
-      # .. line =~ /STOP/
-    end
-    @actual_time = (@end_time - @start_time).floor / 60
-    @log_actual_time = "タスク(#{@task_name})実績時間: #{@actual_time}分}"
-    puts @log_actual_time
+    log_lines = find_task_log
+    @start_time = log_lines[0] 
+    @end_time = log_lines[1] 
+    @actual_time = (@end_time - @start_time).floor/60
+    log_actual_time = "タスク(#{@task_name})実績時間: #{@actual_time}分"
     
     # タスクの実績時間をログファイルに記録
-    record_log(@log_actual_time)
+    record_log(log_actual_time)
   end
 
+  def record_total_work_time
+    
 
+  end
 
   # 本日のタスク一覧（タスク名、開始時間、終了時間、実績時間）と本日の作業合計時間を表示
   def show_today_tasks
     record_actual_time
     puts "本日のタスク一覧"
-
-
-
-    puts "タスク名: #{@task_name}\n開始時間: #{@log_start_time}\n終了時間: #{@log_end_time}\n実績時間: #{@actual_time}分"
+    # puts "タスク名: #{@task_name}\n開始時間: #{@start_time}\n終了時間: #{@end_time}\n実績時間: #{@actual_time}分"
+    log_file = File.open("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt", "r")
+    puts log_file.read
     puts "本日の作業合計時間: "
+    log_file.close      
   end  
-
 
 end
 
@@ -74,9 +79,9 @@ class Option
       when "-f", "--finish"
         record_end_time
       when "-vt", "--view-total"
-        show_todays_tasks
+        view_today_tasks
       when "-vw", "--view-week"
-        show_week_tasks
+        view_week_tasks
       else
        puts "the entered option is not correct, Please follow the instructions below.
        [-s <task_name>] / [--start <task_name>] : Record task start time\n
@@ -96,12 +101,14 @@ class Option
     track_time.record_end_time
   end
 
-  def show_todays_tasks
+  def view_today_tasks
     track_time = TrackTime.new(ARGV[1])
     track_time.show_today_tasks
   end
 
-  def show_week_tasks
+  def view_week_tasks
+    track_time = TrackTime.new(ARGV[1])
+    track_time.show_week_tasks
   end
 end
 
