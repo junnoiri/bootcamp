3cfe7775b4af94db4b048cf56bccf3185e760d3d
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2022-12-26 18:18:28 -0600

    updated the ruby file

--- command-line-tool/time_tracking_tool.rb
+++ command-line-tool/time_tracking_tool.rb
@@ -2,14 +2,15 @@ require 'time'
 
 class TrackTime
   attr_accessor :task_name, :start_time, :end_time, :actual_time
+  ONE_WEEK = 7
 
   def initialize(task_name, start_time: "", end_time: "", actual_time: "")
     @task_name = task_name
     @timestamp = Time.now
   end
 
+  # ログファイルを作成し、記録
   def record_log(work_time)
-    # ログファイルを作成し、記録
     task_log = File.open("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt", "a")
     task_log.puts("#{work_time}")
     task_log.close
@@ -29,13 +30,21 @@ class TrackTime
     puts log_end_time
     # タスクの終了をログファイルに記録
     record_log(log_end_time)
+
+    # タスクの終了時間を記録後、タスクの実績時間を記録
+    record_actual_time
+
+    # タスクの実績時間を記録後、作業合計時間を記録
+    
   end
 
-  def find_task_log
+  def find_today_log
     log_lines = []
     File.foreach("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt"){ |line|
-      line = /: /.match(line).post_match.chomp
-      log_lines << Time.strptime(line,'%Y年%m月%d日 %H時%M分%S秒')
+      if (line.include?("#{@task_name}"))
+        line = /: /.match(line).post_match.chomp
+        log_lines << Time.strptime(line,'%Y年%m月%d日 %H時%M分%S秒')
+      end
     }
     p log_lines
     return  log_lines
@@ -43,7 +52,7 @@ class TrackTime
 
   # タスクの実績時間を記録
   def record_actual_time
-    log_lines = find_task_log
+    log_lines = find_today_log
     @start_time = log_lines[0] 
     @end_time = log_lines[1] 
     @actual_time = (@end_time - @start_time).floor/60
@@ -53,22 +62,44 @@ class TrackTime
     record_log(log_actual_time)
   end
 
+  # 作業合計時間を記録
   def record_total_work_time
-    
+    log_actual_time_lines = []
+    total_work_time = 0
+    File.foreach("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt"){ |line|
+      if (line.include?("実績時間:"))
+        line = /実績時間: /.match(line).post_match.chomp
+        total_work_time += line.to_i
+      end
+    } 
+    log_total_work_time = "作業合計時間: #{total_work_time}分"
 
+    # 作業合計時間をログファイルに記録
+    record_log(log_total_work_time)
   end
 
   # 本日のタスク一覧（タスク名、開始時間、終了時間、実績時間）と本日の作業合計時間を表示
   def show_today_tasks
-    record_actual_time
+    # 作業合計時間を記録
+    record_total_work_time
+    
     puts "本日のタスク一覧"
-    # puts "タスク名: #{@task_name}\n開始時間: #{@start_time}\n終了時間: #{@end_time}\n実績時間: #{@actual_time}分"
     log_file = File.open("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt", "r")
     puts log_file.read
-    puts "本日の作業合計時間: "
-    log_file.close      
-  end  
+    log_file.close
+  end 
 
+  # 直近7日間の日別作業時間を表示
+  def show_week_tasks
+    ONE_WEEK.times { 
+      File.foreach("../log/202212/#{@timestamp.strftime('%Y%m%d')}.txt"){ |line|
+        if (line.include?("作業合計時間:"))
+          puts "#{@timestamp.strftime('%Y年%m月%d日')} #{line}"
+        end
+      } 
+      @timestamp += 60*60*24
+    }
+  end
 end
 
 class Option
